97252f977c6b06b86885829b97cb8bd299de0115
iver56 <iver56@hotmail.com>
2024-01-10 08:45:15 +0100

    Clean up the code a little

--- numpy_minmax/_minmax.c
+++ numpy_minmax/_minmax.c
@@ -1,42 +1,37 @@
 #include <immintrin.h>
+#include <float.h>
 
 typedef struct {
     float min_val;
     float max_val;
 } MinMaxResult;
 
-
 MinMaxResult minmax(float *a, size_t length) {
-    MinMaxResult result;
+    MinMaxResult result = { .min_val = FLT_MAX, .max_val = -FLT_MAX };
 
+    // Return early for empty arrays
     if (length == 0) {
-        result.min_val = 0.0;
-        result.max_val = 0.0;
-        return result;
+        return (MinMaxResult){0.0, 0.0};
     }
-
-    // Initialize min and max with the first element
-    result.min_val = a[0];
-    result.max_val = a[0];
 
     __m256 min_vals = _mm256_set1_ps(result.min_val);
     __m256 max_vals = _mm256_set1_ps(result.max_val);
 
-    // Process eight elements at a time
-    size_t i;
-    for (i = 0; i <= length - 8; i += 8) {
-        __m256 vals = _mm256_loadu_ps(&a[i]); // using loadu for unaligned data
+    // Process elements in chunks of eight
+    size_t i = 0;
+    for (; i <= length - 8; i += 8) {
+        __m256 vals = _mm256_loadu_ps(&a[i]);
         min_vals = _mm256_min_ps(min_vals, vals);
         max_vals = _mm256_max_ps(max_vals, vals);
     }
 
-    // Handling the remainder of the array
+    // Process remainder elements
     for (; i < length; ++i) {
         if (a[i] < result.min_val) result.min_val = a[i];
         if (a[i] > result.max_val) result.max_val = a[i];
     }
 
-    // Extracting results from AVX registers
+    // Reduce min and max values from AVX registers
     float temp_min[8], temp_max[8];
     _mm256_storeu_ps(temp_min, min_vals);
     _mm256_storeu_ps(temp_max, max_vals);
